23543976c872a56b3412f43c8f5f47f1ef88b48c
stanley <stanz_leo@yahoo.com>
2016-08-20 15:13:27 +0700

    first commit

--- src/marathongit4/Marathongit4.java
+++ src/marathongit4/Marathongit4.java
@@ -16,6 +16,7 @@ public class Marathongit4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hello world");
     }
     
 }
